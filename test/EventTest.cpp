c992fe3bf580c6a0585a6eb349ddbf8e711145d4
Giulia <giulia.simoncini2@stud.unifi.it>
2023-08-09 20:50:46 +0200

    Modificato il test del costruttore di Event come conseguenza della modifica degli attributi di Event

--- test/EventTest.cpp
+++ test/EventTest.cpp
@@ -5,12 +5,14 @@
 #include "../Event.h"
 
 TEST(Event, Constructor) {
-    Event e("Cinema", "Vedere_un_film", 12, 3, 2023, 11);
+    Event e("Cinema", "Vedere_un_film", 12.99, 120, 2023, 9, 5, 20, 30);
     ASSERT_EQ("Cinema", e.getName());
     ASSERT_EQ("Vedere_un_film", e.getDescription());
-    ASSERT_EQ(12, e.getPrice());
-    ASSERT_EQ(3, e.getDuration());
-    ASSERT_EQ(2023, e.getDate());
-    ASSERT_EQ(11, e.getTime());
+    ASSERT_EQ(12.99, e.getPrice());
+    ASSERT_EQ(120, e.getDurationInMinutes());
+    ASSERT_EQ(2023, e.getYear());
+    ASSERT_EQ(9, e.getMonth());
+    ASSERT_EQ(5, e.getDay());
+    ASSERT_EQ(20, e.getHourOfBegin());
+    ASSERT_EQ(30, e.getMinuteOfBegin());
 }
-
