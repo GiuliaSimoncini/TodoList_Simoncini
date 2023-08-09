386275905a948d557890942c7a75905d07e93001
Giulia <giulia.simoncini2@stud.unifi.it>
2023-08-09 19:30:16 +0200

    Gestito il caso in cui l'utente inserisca un prezzo negativo e/o una durata negativa

--- Event.cpp
+++ Event.cpp
@@ -47,11 +47,11 @@ int Event::getMinuteOfBegin() const {
 
 Event::Event(const std::string &name, const std::string &description, float price, int durationInMinutes,
              int year, unsigned month, unsigned day, int hourOfBegin, int minuteOfBegin)
-             : name(name), description(description), price(price), durationInMinutes(durationInMinutes){
+             : name(name), description(description){
     std::chrono::year_month_day dateaus{std::chrono::year{year} / std::chrono::month{month} / std::chrono::day{day}};
     while (!dateaus.ok()) {
         std::cout << "Data non valida" << std::endl;
-        std::cout << "Inserire nuovamente l'anno, il mese e il giorno" << std::endl;
+        std::cout << "Inserire nuovamente l'anno, il mese e il giorno dell'evento" << std::endl;
         std::cin >> year;
         std::cin >> month;
         std::cin >> day;
@@ -62,12 +62,26 @@ Event::Event(const std::string &name, const std::string &description, float pric
 
     while (hourOfBegin < 0 || hourOfBegin > 23 || minuteOfBegin < 0 || minuteOfBegin > 59) {
         std::cout << "Orario non valido" << std::endl;
-        std::cout << "Inserire nuovamente l'ora e i minuti" << std::endl;
+        std::cout << "Inserire nuovamente l'ora e il minuto di inizio dell'evento" << std::endl;
         std::cin >> hourOfBegin;
         std::cin >> minuteOfBegin;
     }
     this->hourOfBegin = hourOfBegin;
     this->minuteOfBegin = minuteOfBegin;
+
+    while (price < 0) {
+        std::cout << "Prezzo non valido" << std::endl;
+        std::cout << "Inserire nuovamente il prezzo dell'evento" << std::endl;
+        std::cin >> price;
+    }
+    this->price = price;
+
+    while (durationInMinutes <= 0) {
+        std::cout << "Durata non valida" << std::endl;
+        std::cout << "Inserire nuovamente la durata dell'evento" << std::endl;
+        std::cin >> durationInMinutes;
+    }
+    this->durationInMinutes = durationInMinutes;
 }
 
 void Event::print() const {
